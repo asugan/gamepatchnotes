c23f2c47cbc085dafd339e3ed3aef36e5c0175f6
asugan <cagatayeren1898@gmail.com>
2022-08-17 19:58:02 +0300

    ads

--- resources/views/site/gamepage.blade.php
+++ resources/views/site/gamepage.blade.php
@@ -62,8 +62,8 @@
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4215212273469017"
             crossorigin="anonymous"></script>
         <!-- Yatay -->
-        <ins class="adsbygoogle" style="display:block;" data-ad-client="ca-pub-4215212273469017" data-ad-slot="1904430142"
-            data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <ins class="adsbygoogle mx-auto" style="display:block;" data-ad-client="ca-pub-4215212273469017"
+            data-ad-slot="1904430142" data-ad-format="auto" data-full-width-responsive="true"></ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>

c5b3d1826e3a3274186d3534ba802ad9dd15bd74
asugan <cagatayeren1898@gmail.com>
2022-08-17 19:15:02 +0300

    reklam

--- resources/views/site/gamepage.blade.php
+++ resources/views/site/gamepage.blade.php
@@ -62,9 +62,8 @@
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4215212273469017"
             crossorigin="anonymous"></script>
         <!-- Yatay -->
-        <ins class="adsbygoogle asugan mx-auto overflow-x-auto" style="display:block; border: 1px solid green;"
-            data-ad-client="ca-pub-4215212273469017" data-ad-slot="1904430142" data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
+        <ins class="adsbygoogle" style="display:block;" data-ad-client="ca-pub-4215212273469017" data-ad-slot="1904430142"
+            data-ad-format="auto" data-full-width-responsive="true"></ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>

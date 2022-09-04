e6be6904c14533d0013917664fea43ce0efb0cfd
asugan <cagatayeren1898@gmail.com>
2022-09-04 17:53:59 +0300

    seo

--- resources/views/site/patchnotepage.blade.php
+++ resources/views/site/patchnotepage.blade.php
@@ -18,36 +18,37 @@ $description = Illuminate\Support\Str::replace(['{STEAM_CLAN_IMAGE}', '[img]', '
 @endphp
 
 @extends('layouts.main')
-@section('title', $game->game_name . ' ' . $patchnote->post_title . ' - Latest Patch Notes')
+@section('title', $game->game_name . ' - ' . $patchnote->post_title . ' - Latest Patch Notes')
 @section('description', Str::limit(strip_tags_content($description), 150, '...'))
 @section('keywords', $patchnote->post_title . ',Latestpatchnotes,' . $patchnote->post_title . ' - Latestpatchnotes,' .
     $game->game_name . ' ' . $patchnote->post_title . ' patchnotes')
-@section('og.title', $game->game_name . ' ' . $patchnote->post_title . ' - Latest Patch Notes')
+@section('og.title', $game->game_name . ' - ' . $patchnote->post_title . ' - Latest Patch Notes')
 @section('og.desc', Str::limit(strip_tags_content($description), 150, '...'))
 @section('og.type', 'article')
 @section('og_image', $patchnote->post_image)
 @section('content')
     @php
-    $hamham = Illuminate\Support\Str::replace('{STEAM_CLAN_IMAGE}', 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans', $patchnote->post_body);
-    $hamham2 = Illuminate\Support\Str::replace('[img]', '<img src="', $hamham);
-    $hamham3 = Illuminate\Support\Str::replace('[/img]', '">  ', $hamham2);
-    $hamham4 = Illuminate\Support\Str::replace('[h1]', '<h1 class="text-3xl">', $hamham3);
-    $hamham5 = Illuminate\Support\Str::replace('[/h1]', '</h1>', $hamham4);
-    $hamham6 = Illuminate\Support\Str::replace('[h3]', '<h3 class="text-2xl">', $hamham5);
-    $hamham7 = Illuminate\Support\Str::replace('[/h3]', '</h3>', $hamham6);
-    $hamham8 = Illuminate\Support\Str::replace('[h2]', '<h2 class="text-xl">', $hamham7);
-    $hamham9 = Illuminate\Support\Str::replace('[/h2]', '</h2>', $hamham8);
-    $hamham10 = Illuminate\Support\Str::replace('[b]', '', $hamham9);
-    $hamham11 = Illuminate\Support\Str::replace('[/b]', '', $hamham10);
-    $hamham12 = Illuminate\Support\Str::replace('[list]', '<ul>', $hamham11);
-    $hamham13 = Illuminate\Support\Str::replace('[/list]', '</ul>', $hamham12);
-    $hamham14 = Illuminate\Support\Str::replace('[*]', '<li>', $hamham13);
-    $hamham15 = Illuminate\Support\Str::replace('[hr]', '', $hamham14);
-    $hamham16 = Illuminate\Support\Str::replace('[/hr]', '', $hamham15);
-    $hamham17 = Illuminate\Support\Str::replace(['[url=', ']', '[/url>', '[i>', '[/i>', '[previewyoutube=' , '[/previewyoutube>', ';full>', '[code>', '[/code>', '[/*>', '[olist>', '[/olist>', '[u>', '[/u>'], ['<a href=', '>', '</a>', '<i>', '</i>', 'https://www.youtube.com/watch?v=' , '<br>', '', '', '', '', '<ul>', '</ul>', '<u>', '</u>'], $hamham16);
-
-    $hamham18 = nl2br($hamham17);
-
+        
+        $hamham = Illuminate\Support\Str::replace('{STEAM_CLAN_IMAGE}', 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans', $patchnote->post_body);
+        $hamham2 = Illuminate\Support\Str::replace('[img]', '<img src="', $hamham);
+        $hamham3 = Illuminate\Support\Str::replace('[/img]', '">  ', $hamham2);
+        $hamham4 = Illuminate\Support\Str::replace('[h1]', '<h1 class="text-3xl">', $hamham3);
+        $hamham5 = Illuminate\Support\Str::replace('[/h1]', '</h1>', $hamham4);
+        $hamham6 = Illuminate\Support\Str::replace('[h3]', '<h3 class="text-2xl">', $hamham5);
+        $hamham7 = Illuminate\Support\Str::replace('[/h3]', '</h3>', $hamham6);
+        $hamham8 = Illuminate\Support\Str::replace('[h2]', '<h2 class="text-xl">', $hamham7);
+        $hamham9 = Illuminate\Support\Str::replace('[/h2]', '</h2>', $hamham8);
+        $hamham10 = Illuminate\Support\Str::replace('[b]', '', $hamham9);
+        $hamham11 = Illuminate\Support\Str::replace('[/b]', '', $hamham10);
+        $hamham12 = Illuminate\Support\Str::replace('[list]', '<ul>', $hamham11);
+        $hamham13 = Illuminate\Support\Str::replace('[/list]', '</ul>', $hamham12);
+        $hamham14 = Illuminate\Support\Str::replace('[*]', '<li>', $hamham13);
+        $hamham15 = Illuminate\Support\Str::replace('[hr]', '', $hamham14);
+        $hamham16 = Illuminate\Support\Str::replace('[/hr]', '', $hamham15);
+        $hamham17 = Illuminate\Support\Str::replace(['[url=', ']', '[/url>', '[i>', '[/i>', '[previewyoutube=', '[/previewyoutube>', ';full>', '[code>', '[/code>', '[/*>', '[olist>', '[/olist>', '[u>', '[/u>', '[table>', '[/table>', '[tr>', '[/tr>', '[td>', '[/td>', '[th>', '[/th>'], ['<a href=', '>', '</a>', '<i>', '</i>', 'https://www.youtube.com/watch?v=', '<br>', '', '', '', '', '<ul>', '</ul>', '<u>', '</u>', '<table>', '</table>', '<tr>', '</tr>', '<td>', '</td>', '<th>', '</th>'], $hamham16);
+        
+        $hamham18 = nl2br($hamham17);
+        
     @endphp
 
     <div class="pnheader py-16"
